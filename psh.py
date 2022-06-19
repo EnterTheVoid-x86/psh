cabcf01781f76db939f74df0e4e68db58e94b13f
etvx86 <64978924+EnterTheVoid-x86@users.noreply.github.com>
2022-06-18 20:06:34 -0400

    Update psh.py

--- psh.py
+++ psh.py
@@ -11,19 +11,19 @@ print("Welcome to psh, the open-source shell made in python.")
 username = os.getlogin()
 hostname = socket.gethostname()
 while True:
-		shell = input(username + "@" + hostname + "~$ ")
-		if shell == "exit":
-			print("Exiting...")
-			exit()
-		elif shell == "help":
-			print("psh help:")
-			print("Depending on the OS, any unix or Windows commands are supported.")
-			print("psh built in commands:")
-			print("sysinfo - print system information")
-			print("help - print this help screen")
-			print("exit - exit psh")
-		elif shell == "sysinfo":
-			print("psh v1.2")
-			print("Running on " + platform.system())
-		else:
-			os.system(shell)
+	shell = input(username + "@" + hostname + "~$ ")
+	if shell == "exit":
+		print("Exiting...")
+		exit()
+	elif shell == "help":
+		print("psh help:")
+		print("Depending on the OS, any unix or Windows commands are supported.")
+		print("psh built in commands:")
+		print("sysinfo - print system information")
+		print("help - print this help screen")
+		print("exit - exit psh")
+	elif shell == "sysinfo":
+		print("psh v1.2")
+		print("Running on " + platform.system())
+	else:
+		os.system(shell)
