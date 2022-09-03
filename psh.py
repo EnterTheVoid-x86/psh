1845dddf06c03f3c26305a2d5ecc0b5a48cbae3f
etvx86 <64978924+EnterTheVoid-x86@users.noreply.github.com>
2022-09-03 09:47:49 -0400

    psh v2.0

--- psh.py
+++ psh.py
@@ -1,79 +1,122 @@
-# psh v1.3, rsh ported to python
-# created by etvx86
-import platform, socket, os, sys
-
+import sys
+import os
+import socket
+import platform
 from config import *
+# psh v2.0, rsh ported to python
+# created by etvx86
 
-try:
-	if sys.argv[1] == "--no-logo":
-		ascii = "disabled"
-	elif sys.argv[1] == "--info":
-		print("psh v1.3, rsh ported to python")
-		print("created by etvx86")
-		sys.exit()
-	elif sys.argv[1] == "--version":
-		print("psh v1.3")
-		sys.exit()
+try:    
+    if sys.argv[1] == "--no-logo":
+        ascii = "disabled"
+    elif sys.argv[1] == "--info":
+        print("psh v2.0, rsh ported to python")
+        print("created by etvx86")
+        sys.exit()
+    elif sys.argv[1] == "--version":
+        print("psh v2.0")
+        sys.exit()
+    elif sys.argv[1] == "--debug":
+        debug = True
+    elif sys.argv[1] == "--help":
+        print("psh v2.0 command arguments")
+        print("--no-logo: disable the logo")
+        print("--info: print info about psh")
+        print("--version: print the psh version")
+        print("--debug: enable debug mode")
+        sys.exit()
 except IndexError:
-	pass
+    pass
+
+if platform.system() == "Windows":
+    print("Error: psh cannot be run on Windows!")
+    exit()
 
 asciilogo = open("ascii.txt")
 asciiart = asciilogo.read()
 
-CBLACK  = '\33[30m'
-CRED    = '\33[31m'
-CGREEN  = '\33[32m'
+CBLACK = '\33[30m'
+CRED = '\33[31m'
+CGREEN = '\33[32m'
 CYELLOW = '\33[33m'
-CBLUE   = '\33[34m'
+CBLUE = '\33[34m'
 CVIOLET = '\33[35m'
-CBEIGE  = '\33[36m'
-CWHITE  = '\33[37m'
+CBEIGE = '\33[36m'
+CWHITE = '\33[37m'
 
 if debug == True:
-	print("Debug mode enabled")
-	print("Greeting: " + greeting)
-	print("Prompt: " + prompt)
-	print("Ascii: " + ascii)
-	print("Python version: " + platform.python_version())
-	print("Getting username...")
-	print("Username: " + os.getlogin())
-	print("Getting hostname...")
-	print("Hostname: " + socket.gethostname())
-	print("Getting system...")
-	print("System: " + platform.system())
-	print("[!] PROMPT MAIN CREATION")
-	print("[!] INIT COLORS")
-	print("[!] INIT PROMPT")
+    print("Debug mode enabled")
+    print("Greeting: " + greeting)
+    print("Prompt: " + prompt)
+    print("Ascii: " + ascii)
+    print("Python version: " + platform.python_version())
+    print("Getting username...")
+    print("Username: " + os.getlogin())
+    print("Getting hostname...")
+    print("Hostname: " + socket.gethostname())
+    print("Getting system...")
+    print("System: " + platform.system())
+    print("[!] PROMPT MAIN CREATION")
+    print("[!] INIT COLORS")
+    print("[!] INIT PROMPT")
 
 if ascii == "enabled":
-	print(asciiart)
+    print(asciiart)
 print(greeting)
 
 username = os.getlogin()
 hostname = socket.gethostname()
-while True:
-		shell = input(CBLUE + username + "@" + CYELLOW + hostname + CWHITE + prompt)
-		if shell == "exit":
-			print("Exiting...")
-			sys.exit()
-		elif shell == "help":
-			print("psh help:")
-			print("Depending on the OS, any unix or Windows commands are supported.")
-			print("psh built in commands:")
-			print("sysinfo - print system information")
-			print("pcalc - simple calculator")
-			print("help - print this help screen")
-			print("exit - exit psh")
-		elif shell == "sysinfo":
-			print("psh v1.3")
-			print("Running on " + platform.system())
-		elif shell == "pcalc":
-			print("Type exit to exit.")
-			while True:
-				calc = input(">> ")
-				if calc == "exit":
-					break
-				else:
-					print(eval(calc))
-		else:
-			os.system(shell)
+def main():
+    try:
+        while True:
+            directory = os.getcwd()
+            shell = input(CBLUE + username + CGREEN + "<@>" +
+                  CYELLOW + hostname + CWHITE + "%" + directory + prompt)
+            sharray = shell.split(" ")
+            if "exit" in sharray[0]:
+                print("Exiting...")
+                sys.exit()
+            elif "help" in sharray[0]:
+                print("psh help:")
+                print("Generally speaking, all you need to know is you can use any commands available to you in any other shell like nano.")
+                print("psh built in commands:")
+                print("autocomplete - find commands based on user input")
+                print("sysinfo - print system information")
+                print("pcalc - simple calculator")
+                print("help - print this help screen")
+                print("exit - exit psh")
+            elif "sysinfo" in sharray[0]:
+                print("psh v2.0")
+                print("Running on " + platform.platform())
+            elif "pcalc" in sharray[0]:
+                print("Type exit to exit.")
+                while True:
+                    calc = input(">> ")
+                    if calc == "exit":
+                        break
+                    else:
+                        print(eval(calc))
+            elif "autocomplete" in sharray[0]:
+                print("Type in the start of some commands, we will find a command with that string included.")
+                while True:
+                    autoinput = input(">> ")
+                    if autoinput == "exit":
+                        break
+                    else:
+                        os.system("ls /usr/bin/ | grep " + autoinput)
+            elif "cd" in sharray[0]:
+                try:
+                    os.chdir(sharray[1])
+                except FileNotFoundError:
+                    print("Invalid directory.")
+                except IndexError:
+                    print("Please input a directory.")
+            else:
+                os.system(shell)
+    except KeyboardInterrupt:
+        print("")
+        main()
+    except EOFError:
+        print("\nExiting...")
+
+main()
