325f21fd8c5c49efb88a80f389762704306bfd25
etvx86 <64978924+EnterTheVoid-x86@users.noreply.github.com>
2022-09-03 13:27:19 -0400

    psh v2.1
    Added help on specific commands. CD now works.

--- psh.py
+++ psh.py
@@ -3,23 +3,23 @@ import os
 import socket
 import platform
 from config import *
-# psh v2.0, rsh ported to python
+# psh v2.1, rsh ported to python
 # created by etvx86
 
 try:    
     if sys.argv[1] == "--no-logo":
         ascii = "disabled"
     elif sys.argv[1] == "--info":
-        print("psh v2.0, rsh ported to python")
+        print("psh v2.1, rsh ported to python")
         print("created by etvx86")
         sys.exit()
     elif sys.argv[1] == "--version":
-        print("psh v2.0")
+        print("psh v2.1")
         sys.exit()
     elif sys.argv[1] == "--debug":
         debug = True
     elif sys.argv[1] == "--help":
-        print("psh v2.0 command arguments")
+        print("psh v2.1 command arguments")
         print("--no-logo: disable the logo")
         print("--info: print info about psh")
         print("--version: print the psh version")
@@ -77,16 +77,36 @@ def main():
                 print("Exiting...")
                 sys.exit()
             elif "help" in sharray[0]:
-                print("psh help:")
-                print("Generally speaking, all you need to know is you can use any commands available to you in any other shell like nano.")
-                print("psh built in commands:")
-                print("autocomplete - find commands based on user input")
-                print("sysinfo - print system information")
-                print("pcalc - simple calculator")
-                print("help - print this help screen")
-                print("exit - exit psh")
+                try:
+                    if sharray[1] == "autocomplete":
+                        print("autocomplete v1.0.0")
+                        print(CWHITE + "A psh built-in command to find a command based on user input.")
+                        print("USAGE: Run the command. You will be asked to input a few letters that are included in the command. It will find commands that have your input in them.")
+                        main()
+                    elif sharray[1] == "sysinfo":
+                        print("sysinfo v2.1")
+                        print(CWHITE + "A psh built-in command to display system information.")
+                        print("USAGE: Run the command.")
+                        main()
+                    elif sharray[1] == "pcalc":
+                        print("pcalc v1.3")
+                        print(CWHITE + "A psh built-in command to do simple calculations.")
+                        print("USAGE: Run the command and type in a problem. The shell will do the math for you.")
+                        main()
+                    elif sharray[1] == "help":
+                        print("I dont think you need help on this one.")
+                        main()
+                except IndexError:
+                    print("psh help:")
+                    print("Generally speaking, all you need to know is you can use any commands available to you in any other shell like nano.")
+                    print("psh built in commands:")
+                    print("autocomplete - find commands based on user input")
+                    print("sysinfo - print system information")
+                    print("pcalc - simple calculator")
+                    print("help - print this help screen")
+                    print("exit - exit psh")
             elif "sysinfo" in sharray[0]:
-                print("psh v2.0")
+                print("psh v2.1")
                 print("Running on " + platform.platform())
             elif "pcalc" in sharray[0]:
                 print("Type exit to exit.")
@@ -118,5 +138,5 @@ def main():
         main()
     except EOFError:
         print("\nExiting...")
-
+        sys.exit()
 main()
